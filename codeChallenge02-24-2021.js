12ba018870817e0cce0a4abbdb10dec72742ffd6
anthonymjimenez <anthonyjim96@gmail.com>
2021-02-24 23:19:42 -0500

    twoSum challenge

--- codeChallenge02-24-2021.js
+++ codeChallenge02-24-2021.js
@@ -18,4 +18,40 @@ const merge = function (intervals) {
 }
 
   
-  console.log(merge([[1,3],[2,6],[8,10],[15,18]] ))
+  //console.log(merge([[1,3],[2,6],[8,10],[15,18]] ))
+
+  // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+var twoSum = function(nums, target) {
+    let newArray = [];
+    
+    nums.forEach((num, index, srcArray) => {
+        let counter = 0; 
+        while(counter <= srcArray.length) {
+            if(counter === index) return; 
+            if(num + srcArray[counter] == target) {
+                newArray.push(index, counter);
+                break;
+            } else { counter++; }
+        }
+    })
+return newArray
+}
+
+var twoSum = function(nums, target) {
+    let lookUp = {}
+    for(let x = 0; x < nums.length; x++) {
+        lookUp[x] = nums[x]
+    }
+
+    for(let x = 0; x < nums.length; x++) {
+        let desired = target - nums[x]
+        if(Object.values(lookUp).includes(desired) && Object.keys(lookUp).find(key => lookUp[key] === desired) != x) {
+            return [x,  Object.keys(lookUp).find(key => lookUp[key] === desired)]
+        }
+    }
+}
+ console.log(twoSum([3,2,4],6))
